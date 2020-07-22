2e058815ad45964870dd4c93a13af2ab81f8e2f8
unknown <nelsonsaakekofi@gmail.com>
2020-07-22 17:41:28 +0000

    commit changes

--- app/src/main/java/com/example/roomwordsample/WordViewModel.kt
+++ app/src/main/java/com/example/roomwordsample/WordViewModel.kt
@@ -3,6 +3,9 @@ package com.example.roomwordsample
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class WordViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -14,5 +17,9 @@ class WordViewModel(application: Application) : AndroidViewModel(application) {
         val wordsDao = WordRoomDatabase.getDatabase(application).wordDao()
         repository = WordRepository(wordsDao)
         allWords = repository.allWords
+    }
+
+    fun insert(word: Word) = viewModelScope.launch(Dispatchers.IO) {
+        repository.insert(word)
     }
 }
